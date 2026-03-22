93a11375db3ffd0b26c295e2297a2bb174b384a0
mrmerzoukimustafa-oss <mr.merzoukimustafa@gmail.com>
2026-03-22 18:43:18 +0100

    Delete App.py

--- App.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import math
-import io
-from datetime import datetime
-
-# Imports conditionnels avec gestion d'erreur
-try:
-    import openpyxl
-    from openpyxl.styles import Font, PatternFill
